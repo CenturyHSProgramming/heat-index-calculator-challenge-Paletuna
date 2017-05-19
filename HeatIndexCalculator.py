b5d7946b05ea9bc7a0dc102c981a75145de683f2
Schlitzkus_Rowan <schlr284@hsd.k12.or.us>
2017-05-19 11:10:03 -0700

    adding successful code

--- HeatIndexCalculator.py
+++ HeatIndexCalculator.py
@@ -6,7 +6,10 @@
 
 # Define Function below
 # be sure to return an integer
-
+import math
+def calculateHeatIndex(relHumidity, temp):
+    heatIndex = -42.379 + (2.04901523*temp) + (10.141333127*relHumidity)-(.22475541*temp*relHumidity) - (.00683783*temp*temp)-(.05481717*relHumidity*relHumidity)+(.00122874*temp*temp*relHumidity)+ (.00085282*temp*relHumidity*relHumidity) - (.00000199*temp*temp*relHumidity*relHumidity)
+    return round(heatIndex, 0)
 
 if __name__ == '__main__':
     # Call the function in here if you want to test it
